32e7bf653a49e49f5f3eb3d327e85acbd2bee5b1
femakin <oluwafemi.akinyemi@datamellon.com>
2022-02-04 08:18:30 +0100

    First implementation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'
+
+export default function Navbar() {
+  return <div>
+
+    <div className="navbar-body">
+        <h1>Navbar</h1>
+    </div>
+
+  </div>;
+}
